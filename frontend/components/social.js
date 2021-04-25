6cf69e9d1764c622f56b66929c7e44f70fe057a9
Kevin He <decade901@gmail.com>
2021-04-25 23:56:30 +1000

    changed social media data structure

--- frontend/components/social.js
+++ frontend/components/social.js
@@ -4,15 +4,19 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 export default function Social({ ...props }) {
   return (
     <div className={socialStyles.container}>
-      <a href={'https://github.com/' + props.social.github} className={socialStyles.iconContainer} target="_blank" rel="noopener noreferrer">
-        <FontAwesomeIcon icon={['fab', 'github']}  className={socialStyles.icon}/>
-      </a> 
-      <a href={'https://linkedin.com/in/' + props.social.linkedin} className={socialStyles.iconContainer} target="_blank" rel="noopener noreferrer">
+      {
+        props.social.map((social) =>
+          <a href={social.url} className={socialStyles.iconContainer} target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={['fab', social.name]}  className={socialStyles.icon}/>
+          </a> 
+        )
+      }
+      {/* <a href={'https://linkedin.com/in/' + props.social.linkedin} className={socialStyles.iconContainer} target="_blank" rel="noopener noreferrer">
         <FontAwesomeIcon icon={['fab', 'linkedin']} className={socialStyles.icon}/>
       </a>
       <a href={'https://www.instagram.com/' + props.social.instagram} className={socialStyles.iconContainer} target="_blank" rel="noopener noreferrer">
         <FontAwesomeIcon icon={['fab', 'instagram']} className={socialStyles.icon}/>
-      </a>
+      </a> */}
       <a href={`mailto:${props.social.email}`} className={socialStyles.iconContainer} target="_blank" rel="noopener noreferrer">
         <FontAwesomeIcon icon={['fas', 'envelope-square']} className={socialStyles.icon}/>
       </a>

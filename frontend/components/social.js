b6ba1fe55b6d6d3d102c6171a804ef0d7c30cde8
Kevin He <decade901@gmail.com>
2021-04-26 00:04:41 +1000

    updated social and added mixins

--- frontend/components/social.js
+++ frontend/components/social.js
@@ -11,16 +11,9 @@ export default function Social({ ...props }) {
           </a> 
         )
       }
-      {/* <a href={'https://linkedin.com/in/' + props.social.linkedin} className={socialStyles.iconContainer} target="_blank" rel="noopener noreferrer">
-        <FontAwesomeIcon icon={['fab', 'linkedin']} className={socialStyles.icon}/>
-      </a>
-      <a href={'https://www.instagram.com/' + props.social.instagram} className={socialStyles.iconContainer} target="_blank" rel="noopener noreferrer">
-        <FontAwesomeIcon icon={['fab', 'instagram']} className={socialStyles.icon}/>
-      </a> */}
       <a href={`mailto:${props.social.email}`} className={socialStyles.iconContainer} target="_blank" rel="noopener noreferrer">
         <FontAwesomeIcon icon={['fas', 'envelope-square']} className={socialStyles.icon}/>
       </a>
     </div>
-    // </section>
   )
 }
